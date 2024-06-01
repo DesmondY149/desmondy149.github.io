23248872d4ad7812fa048262a6bd347e7868eed9
DesmondY149 <desmond.149@outlook.com>
2024-06-01 09:15:43 -0600

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,40 @@
+// const myHeading = document.querySelector("h1");
+// myHeading.textContent = "Hello world!";
+
+// document.querySelector("html").addEventListener("click", function () {
+//     alert("Ouch! Stop poking me!");
+//   });
+
+// document.querySelector("html").addEventListener("click", () => {
+//      alert("Ouch! Stop poking me!");
+//    });
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/firefox-icon.png") {
+        myImage.setAttribute("src", "images/firefox2.png")
+    } else {
+        myImage.setAttribute("src", "images/firefox-icon.png");
+    }
+}
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+        //setUserName();
+        return;
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
+
+
+myButton.onclick = () => {
+    setUserName();
+};
